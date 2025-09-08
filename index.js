1475fb6cd3efd476404b6916c8ed8f4295e6fe1b
unknown <mijan90786@gmail.com>
2025-09-08 14:56:27 +0600

    add all trees dynamicly

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 console.log("connected")
 const categoriesContainer = document.getElementById("categories-container")
 const itemsContainer = document.getElementById("items-container")
@@ -10,7 +12,6 @@ const loadCategories = () =>{
 }
 
 const displayCategories = (categories) =>{
-    
     categories.forEach(cate => {
         categoriesContainer.innerHTML += `<li class="categories text-base font-medium hover:bg-green-400 list-none mb-4 rounded-lg p-1 " id="${cate.id}">${cate.category_name} </li>`
        
@@ -26,6 +27,7 @@ const displayCategories = (categories) =>{
                 activeCat.classList.add("bg-green-900")
                 activeCat.classList.add("text-white")
                 loadItems(e.target.id)
+                // console.log(e.target.id)
             }
         })
 }
@@ -33,7 +35,15 @@ const displayCategories = (categories) =>{
 
 // load items 
 
+
 const loadItems= (id) =>{
+    if( id ==="plants" ){
+       const url = `https://openapi.programming-hero.com/api/plants`
+       fetch(url)
+       .then(res=>res.json())
+       .then(data=> displayItems(data.plants))
+        return ;
+    }
     const url = `https://openapi.programming-hero.com/api/category/${id} `
     fetch(url)
     .then(res => res.json())
@@ -43,10 +53,33 @@ const loadItems= (id) =>{
 // display items 
 
 const displayItems = (items) =>{
+  
+    itemsContainer.innerHTML = "";
     items.forEach(item =>{
-        console.log(item)
+
+        const newItem = document.createElement("div")
+        newItem.innerHTML = `   <div class=" rounded-xl bg-[whitesmoke] h-full ">
+                        <div class="">
+                            <img src="${item.image}" alt="${item.name} photo"  class=" w-full h-50 object-cover rounded-t-xl">
+                        </div>
+                        <div class="p-3">
+                            <h2 class=" capitalize text-2xl font-bold item-name "> ${item.name}</h2>
+                            <p class="text-lg font-normal">${item.description}</p>
+                            <div class="flex justify-between">
+                                <span class="bg-[#DCFCE7] p-2 text-green-500 rounded-xl"> ${item.category}</span>
+                                <p> <span> ${item.price} </span>  BDT </p>
+                            </div>
+                            <div>
+                                <button class="w-full rounded-xl bg-green-700 p-3"> add to cart </button>
+                            </div>
+                        </div>
+                    </div>`
+
+                    itemsContainer.append(newItem)
     })
 }
+
+
 
 
 
