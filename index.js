ebcd7df7c4c8a7090d78b1c9f55c0facb9a73d1f
unknown <mijan90786@gmail.com>
2025-09-09 08:35:09 +0600

    add cart delete function

--- index.js
+++ index.js
@@ -7,73 +7,71 @@ const modalContainer = document.getElementById("modal-container");
 const cartContainer = document.getElementById("cart-container");
 const totalPrice = document.getElementById("total-price")
 
-const manageSpinner = ((status) =>{
-    if(status === true){
+// add spinner  here 
+const manageSpinner = ((status) => {
+    if (status === true) {
         document.getElementById("spinner").classList.remove("hidden")
         itemsContainer.classList.add("hidden")
     }
-    else{
+    else {
         document.getElementById("spinner").classList.add("hidden")
         itemsContainer.classList.remove("hidden")
     }
-  
+
 })
 
-const loadCategories = () =>{
+// load categories 
+const loadCategories = () => {
     const url = "https://openapi.programming-hero.com/api/categories"
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
 }
 
-const displayCategories = (categories) =>{
+// displayCategories function here 
+const displayCategories = (categories) => {
     categories.forEach(cate => {
         categoriesContainer.innerHTML += `<li class="categories text-base font-medium hover:bg-green-400 list-none mb-4 rounded-lg p-1 " id="${cate.id}">${cate.category_name} </li>`
-       
+
     });
-     categoriesContainer.addEventListener("click" ,(e) =>{
-            if(e.target.className.includes("categories")){
-                const allCat = document.querySelectorAll(".categories")
-                allCat.forEach(el => {
-                    el.classList.remove("bg-green-900")
-                    el.classList.remove("text-white")
-                });
-                const activeCat = e.target
-                activeCat.classList.add("bg-green-900")
-                activeCat.classList.add("text-white")
-                loadItems(e.target.id)
-                // console.log(e.target.id)
-            }
-        })
+    // function for active button 
+    categoriesContainer.addEventListener("click", (e) => {
+        if (e.target.className.includes("categories")) {
+            const allCat = document.querySelectorAll(".categories")
+            allCat.forEach(el => {
+                el.classList.remove("bg-green-900")
+                el.classList.remove("text-white")
+            });
+            const activeCat = e.target
+            activeCat.classList.add("bg-green-900")
+            activeCat.classList.add("text-white")
+            loadItems(e.target.id)
+
+        }
+    })
 }
 
 
-// load items 
-
-
-const loadItems= (id) =>{
+// load items by categories function
+const loadItems = (id) => {
     manageSpinner(true)
-    if( id ==="plants" ){
-       const url = `https://openapi.programming-hero.com/api/plants`
-       fetch(url)
-       .then(res=>res.json())
-       .then(data=> displayItems(data.plants))
-        return ;
+    // for defult items 
+    if (id === "plants") {
+        const url = `https://openapi.programming-hero.com/api/plants`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayItems(data.plants))
+        return;
     }
     const url = `https://openapi.programming-hero.com/api/category/${id} `
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayItems(data.plants))
+        .then(res => res.json())
+        .then(data => displayItems(data.plants))
 }
-
-// display items 
-
-const displayItems = (items) =>{
-  
+// display items by categories
+const displayItems = (items) => {
     itemsContainer.innerHTML = "";
-    items.forEach(item =>{
-        // console.log(item.id)
-
+    items.forEach(item => {
         const newItem = document.createElement("div")
         newItem.innerHTML = `   <div class=" rounded-xl bg-[whitesmoke] shadow-lg h-full">
                         <div class="">
@@ -92,53 +90,52 @@ const displayItems = (items) =>{
                         </div>
                     </div>`
 
-                    itemsContainer.append(newItem)
+        itemsContainer.append(newItem)
 
     })
     manageSpinner(false)
 
 }
-
-
-
-      itemsContainer.addEventListener("click",(e) =>{
-    if(e.target.className.includes("item-name")){
-       const title = e.target.parentNode.id
-       loadTreeDetails(title)
+//  addEventListener for cart click and titleclick 
+itemsContainer.addEventListener("click", (e) => {
+    if (e.target.className.includes("item-name")) {
+        const title = e.target.parentNode.id
+        loadTreeDetails(title)
     }
-    if(e.target.className.includes("cart-btn")){{
-        const button = e.target 
-        const title = button.parentNode.parentNode.children[0].innerText
-        const price = button.parentNode.parentNode.children[2].children[1].children[0].innerText
-        cartContainer.innerHTML +=` <div class=" p-2 " >
-                            <div class="flex bg-gray-50 rounded-xl justify-between p-2 items-center ">
-                                <div> 
+    if (e.target.className.includes("cart-btn")) {
+        {
+            const button = e.target
+            const title = button.parentNode.parentNode.children[0].innerText
+            const price = button.parentNode.parentNode.children[2].children[1].children[0].innerText
+            cartContainer.innerHTML += ` <div class=" p-2 " >
+                            <div class="flex bg-gray-100 rounded-xl justify-between p-2 items-center ">
+                                <div class="pl-1"> 
                                     <h1 class="text-xl font-bold"> ${title} </h1>
                                     <p > <span id=""> ${price}</span> BDT </p>
                                 </div>
-                                <div class="text-red-800 text-2xl "> 
+                                <div class="text-red-800 text-2xl pr-1"> 
                                     <i class="fa-solid fa-xmark deleteBtn"></i>
                                 </div>
                             </div>
                         </div>`
 
-        currentPrice = Number(totalPrice.innerText) + Number(price)
-        totalPrice.innerText = currentPrice ;
+            currentPrice = Number(totalPrice.innerText) + Number(price)
+            totalPrice.innerText = currentPrice;
 
-    }}
+        }
+    }
 })
-
-const loadTreeDetails = (id)=>{
+// load deatails for title click 
+const loadTreeDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/plant/${id}`
     fetch(url)
-    .then(res=> res.json())
-    .then(data => showTreeDetails(data.plants))
+        .then(res => res.json())
+        .then(data => showTreeDetails(data.plants))
 }
-
-const showTreeDetails = (plant =>{
+//  display modal 
+const showTreeDetails = (plant => {
     plant_tree_modal.showModal();
-
-    modalContainer.innerHTML =`<h3 class="text-xl font-bold mb-2 "> ${plant.name}</h3>
+    modalContainer.innerHTML = `<h3 class="text-xl font-bold mb-2 "> ${plant.name}</h3>
         <div class=" rounded-xl mb-2"> 
             <img src="${plant.image}" alt="${plant.name} photo" class="w-full h-80  object-cover rounded-xl">
         </div>
@@ -149,15 +146,20 @@ const showTreeDetails = (plant =>{
         </div>`
 })
 
+// addEventListener for cart items delete 
+cartContainer.addEventListener("click", (e) => {
+    if (e.target.className.includes("deleteBtn")) {
+        // const allDeleteBtn = document.querySelectorAll(".deleteBtn");
+        const activeDeleteBtn = e.target.parentNode.parentNode.parentNode
+        cartContainer.removeChild(activeDeleteBtn)
+        // console.log(activeDeleteBtn)
+        const price = e.target.parentNode.parentNode.children[0].children[1].children[0].innerText
+        currentPrice = Number(totalPrice.innerText ) - Number(price)
+        totalPrice.innerText = Number(currentPrice)
 
-cartContainer.addEventListener("click", (e) =>{
-    if(e.target.className.includes("deleteBtn")){
-        const deleteBtn = e.target;
-        
     }
-    // console.log(e.target)
 })
 
-
+// call function for display default categories
 loadCategories();
 loadItems('plants');
